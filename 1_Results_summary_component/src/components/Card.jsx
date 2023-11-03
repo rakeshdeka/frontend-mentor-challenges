82a4faec59b1f05251a290a96680e60a64875969
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-11-03 16:31:42 +0530

    design the layout of summary component

---
+++ 1_Results_summary_component/src/components/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./card.css"
+
+export const Card = () => {
+    return (
+        <>
+            <div className='body'>
+                <div id="component-container">
+                    <div className="result-container inner"></div>
+                    <div className="summary-container inner">
+                        <h3>Summary</h3>
+                        <div className="score-container">
+                            <div className="score-div div0">score-div1</div>
+                            <div className="score-div div1">score-div2</div>
+                            <div className="score-div div2">score-div3</div>
+                            <div className="score-div div3">score-div4</div>
+                        </div>
+                        <div className="btn">Continue<div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
