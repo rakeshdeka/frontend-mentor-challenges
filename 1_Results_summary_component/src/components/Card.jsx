3d5de91ecff782ddeabd7154fc4b83bbf32b59d5
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-11-03 22:50:13 +0530

    design the layout

--- 1_Results_summary_component/src/components/Card.jsx
+++ 1_Results_summary_component/src/components/Card.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
-import "./Card.css"
+
 
 export const Card = () => {
     return (
         <>
-            <div className='body'>
-                <div id="component-container">
-                    <div className="result-container inner"></div>
-                    <div className="summary-container inner">
-                        <h3>Summary</h3>
-                        <div className="score-container">
-                            <div className="score-div div0">score-div1</div>
-                            <div className="score-div div1">score-div2</div>
-                            <div className="score-div div2">score-div3</div>
-                            <div className="score-div div3">score-div4</div>
+            <div className="h-[100vh] flex justify-center items-center ">
+                <div className='flex  justify-center items-center  w-[600px] h-[500px] rounded-2xl shadow-2xl'>
+                    <div className=' flex flex-col h-[100%] w-[50%]  rounded-2xl  bg-gradient-to-b from-[#7857ff] to-[#2e2be9] justify-center items-center'>
+                        <h1 className='text-center'>You result</h1>
+                        <div className='flex flex-col justify-center items-center rounded-full w-[150px] h-[150px] bg-gradient-to-b from-[#4e21caff] to-[#2421ca00] shadow-2xl'>
+                            <p className='text-5xl font-semibold text-white'>76</p>
+                            <p className='text-[#737373] font-semibold text-sm'>of 100</p>
                         </div>
-                        <div className="btn">Continue<div>
-                        </div>
-                        </div>
-                    </div>
-                </div>
+                        <p className='text-center text-lg font-bold'>Great</p>
+                        <p className='text-center'>You scored higher than 65% of the people who have taken these tests  </p>
 
-            </div>
+                    </div>
+                    <div className='h-[100%] w-[50%] '>summary</div>
+                </div>
+            </div >
+
         </>
     )
 }
