9c17957ade85411260525fefbfba5a1127c9d150
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-11-09 08:23:34 +0530

    changes in responsivness

--- 2_Product_preview_card_component/src/App.jsx
+++ 2_Product_preview_card_component/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className="h-[100vh] w-[100wh] flex justify-center items-center sm:bg-[#f2ebe3] ">
-        <div className="h-[600px] w-[300px] flex flex-col  shadow-lg rounded-b-2xl sm:w-[40%] sm:h-[60%] ">
+        <div className="h-[600px] w-[300px] flex flex-col  shadow-lg rounded-b-2xl sm:w-[40%] sm:h-[60%] sm:bg-[grey]  md:bg-[red] lg:bg-[yellow] xl:bg-[green]">
           <div className="h-[100%] w-[100%] flex flex-col sm:flex-row ">
             {/* <div className="h-[100%] w-[200px] sm:visible  ">
               <img src={image} alt="img" className="h-[100%] bg-[violet] rounded-l-xl" />
@@ -21,20 +21,20 @@ function App() {
             <div className="h-[60%] w-[100%] flex justify-center sm:h-full border sm:rounded-r-2xl">
               <div className="h-[95%] w-[90%]  flex flex-col items-center  mt-3">
                 <div className="h-[10%] w-[100%]  flex ">
-                  <h1 className="text-[#6c7289] font-montserrat text-[.8rem] tracking-widest">PERFUME</h1>
+                  <h1 className="text-[#6c7289] font-montserrat text-[.7rem] tracking-widest">PERFUME</h1>
                 </div>
                 <div className="h-[25%] w-[100%] flex sm:h-[30%] ">
-                  <p className="font-bold font-fraunces text-3xl text-[#1c232b]">Gabrielle Essence Eau De Parfume</p>
+                  <p className="font-bold font-fraunces text-[1.6rem] text-[#1c232b]">Gabrielle Essence Eau De Parfume</p>
                 </div>
                 <div className="h-[25%] w-[100%] flex ">
-                  <p className="text-[14px] text-[#6c7289] font-md font-montserrat">A floral, solar and voluptuous iterpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+                  <p className="text-[.8rem] text-[#6c7289] font-md font-montserrat">A floral, solar and voluptuous iterpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
                 </div>
                 <div className="h-[15%] w-[90%]  flex gap-3 ">
                   <div className="h-[100%]  flex items-center">
-                    <p className="font-fraunces text-3xl text-[#3c8067]">$149.99</p>
+                    <p className="font-fraunces text-xl text-[#3c8067]">$149.99</p>
                   </div>
                   <div className="h-[100%] flex items-center">
-                    <p className="text-[.8rem] text-[#6c7289] line-through">$169.99</p>
+                    <p className="text-[.6rem] text-[#6c7289] line-through">$169.99</p>
                   </div>
                 </div>
                 <div className="h-[12.5%] w-[95%] font-bold text-sm bg-[#3c8067] text-white flex justify-center items-center rounded-lg cursor-pointer hover:bg-[#103b2b] gap-2">
