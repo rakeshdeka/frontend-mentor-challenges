aebc6b2b67712acbf95ffdf8beed3aba66cdd382
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-11-09 10:30:55 +0530

    refector the design

--- 2_Product_preview_card_component/src/App.jsx
+++ 2_Product_preview_card_component/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
                 <div className="h-[25%] w-[100%] flex sm:h-[30%]">
                   <p className="text-[.85rem] text-[#6c7289] font-md font-montserrat sm:text-[.75rem] sm:leading-5 sm:pt-2">A floral, solar and voluptuous iterpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
                 </div>
-                <div className="h-[15%] w-[90%]  flex gap-3 pb-2">
+                <div className="h-[15%] w-[100%]  flex gap-3 pb-2">
                   <div className="h-[100%]  flex items-center">
                     <p className="font-fraunces text-3xl sm:text-2xl text-[#3c8067]">$149.99</p>
                   </div>
@@ -35,7 +35,7 @@ function App() {
                     <p className="text-[.8rem] text-[#6c7289] line-through">$169.99</p>
                   </div>
                 </div>
-                <div className="h-[12.5%] w-[95%] font-bold text-sm bg-[#3c8067] text-white flex justify-center items-center rounded-lg cursor-pointer hover:bg-[#103b2b] gap-2">
+                <div className="h-[12.5%] w-[100%] font-bold text-sm bg-[#3c8067] text-white flex justify-center items-center rounded-lg cursor-pointer hover:bg-[#103b2b] gap-2">
                   <img src={iconCart} alt="" />
                   <p>Add to Cart</p>
                 </div>
